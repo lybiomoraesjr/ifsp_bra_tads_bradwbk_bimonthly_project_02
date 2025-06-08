0551605947a0f26cdc2f3b05c880b0f9de0faf99
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-08 20:06:35 -0300

    chore: setup dotenv

--- src/main/java/com/codexasistemas/todoapp/api/ApiApplication.java
+++ src/main/java/com/codexasistemas/todoapp/api/ApiApplication.java
@@ -3,10 +3,16 @@ package com.codexasistemas.todoapp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
