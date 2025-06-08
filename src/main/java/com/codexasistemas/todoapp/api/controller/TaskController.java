10b88371b193ce9c742e067c9e4b269628365824
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-07 22:00:48 -0300

    refactor: use autowired

--- src/main/java/com/codexasistemas/todoapp/api/controller/TaskController.java
+++ src/main/java/com/codexasistemas/todoapp/api/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.codexasistemas.todoapp.api.dto.task.TaskResponseDto;
 import com.codexasistemas.todoapp.api.service.interfaces.TaskService;
 
 import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +17,8 @@ import java.util.List;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
-    private final TaskService taskService;
-
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    @Autowired
+    private TaskService taskService;
 
     @GetMapping
     public List<TaskResponseDto> findAll() {
